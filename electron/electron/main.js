c1f5b312b5b74470e2e8de7af1f36e5e905f39b7
Hernanqy <desarrolladoryotti@gmail.com>
2026-06-24 09:54:16 -0300

    dfgd

---
+++ electron/electron/main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    minWidth: 1024,
+    minHeight: 700,
+    backgroundColor: "#000000",
+    autoHideMenuBar: true,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  const indexPath = path.join(__dirname, "../out/index.html");
+
+  win.loadFile(indexPath);
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
